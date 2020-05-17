2161dd8d94c73ea3b7ca38f712fdb62f296a31ef
Isaac Bandim <ibandim@outlook.com>
2020-05-17 07:13:52 -0300

    Outro exercício; Another exercise
    Esse exercício fiz para pesquisar se um elemento está dentro de um vetor.
    Se estiver vai aparecer com uma resposta no console, se não aparecerá de outra.

---
+++ script01.js
@@ -0,0 +1,19 @@
+//Verificar se var possui a habilidade "Javascript"
+//e retorna um booleano true/false caso exista ou não
+
+var skills = ["Javascript", "React", "React Native"]
+
+
+var elemento = skills.indexOf("Javascript") //Elemento de procura de vetor
+
+if(elemento == 0){
+
+    console.log('Há Javascript na Skills')    
+
+}else{
+    console.log('Não há')
+}
+
+
+
+  
