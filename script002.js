0f662b7d6671207b2646eabed7194e96e3f24ba3
Isaac Bandim <ibandim@outlook.com>
2020-05-19 23:26:09 -0300

    Lista de Nomes e opção de inserir eles
    adicionei um input em tela e um botão como a seguir:
    Ao clicar no botão, a função adicionar() deve ser disparada adicionando um novo item a lista de
    nomes baseado no nome preenchido no input e renderizando o novo item em tela juntos aos
    demais itens anteriores. Além disso, o conteúdo do input deve ser apagado após o clique.

---
+++ script002.js
@@ -0,0 +1,52 @@
+/*
+Preencha uma lista (<ul>) no HTML com os itens da seguinte forma:
+● Diego
+● Gabriel
+● Lucas
+*/
+
+var nomes = [ "Diego", "Gabriel", "Lucas"];
+nomes.length
+console.log(nomes);
+  
+
+function CreateElement(){
+
+
+        for(var nome of nomes){ 
+                
+                var liElement = document.createElement('li');
+                var liText = document.createTextNode(nome);
+                liElement.appendChild(liText);     
+                
+                var place = document.querySelector('ul')
+                place.appendChild(liElement)
+
+
+                //console.log(liElement);
+        }
+        
+}
+ //const add 
+ function adicionar(){
+
+      
+         var read = document.querySelector('input')
+         
+         nomes.push(read.value)//Adicionar Elemento ao vetor
+         var liElement = document.createElement('li')
+         var liText = document.createTextNode(read.value)
+         liElement.appendChild(liText)
+         var place = document.querySelector('ul')
+         place.appendChild(liElement)
+        
+         function reset(){
+                 document.querySelector('input')
+                 read.value = ''
+         }
+         reset()
+
+         console.log(nomes) 
+         
+ }
+CreateElement()
