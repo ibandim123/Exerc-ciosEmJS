8113e7967dfebfd28da31739a4c9e7118a660c37
Isaac Bandim <ibandim@outlook.com>
2020-06-12 22:20:35 -0300

    Async Await increment

--- setTimeout.js
+++ setTimeout.js
@@ -12,7 +12,22 @@ function dobrarEm5Segundos(x) {
     });
         
   }
-console.log(dobrarEm5Segundos(5))
+
+async function receive(x){
+    
+    let a = await dobrarEm5Segundos(10)
+ 
+    let b = await dobrarEm5Segundos(20)
+    
+    let c = await dobrarEm5Segundos(30)
+
+    return x + a + b + c;
+}
+
+receive(10).then((sum) => {
+console.log(sum);
+});
+
 
 
 
