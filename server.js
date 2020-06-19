59de98d774e9940196d6794a33469fa81c95d1cf
Isaac Bandim <ibandim@outlook.com>
2020-06-19 12:09:11 -0300

    Add Gate and Router with JS.

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');//call express 
+
+const app = express() // execute function express
+
+app.get('/',(req,res) => {}) // route , req is "requisição" and res is "resposta"
+
+app.listen(3001) // the gate ear
